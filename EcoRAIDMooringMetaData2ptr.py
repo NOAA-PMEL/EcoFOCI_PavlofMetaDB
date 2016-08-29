d4b639eff81383b0e25c8dd6c91c0ff293236eea
shaunwbell <shaun.bell@noaa.gov>
2016-08-29 12:23:19 -0700

    begin building ptr file creator

--- EcoRAIDMooringMetaData2ptr.py
+++ EcoRAIDMooringMetaData2ptr.py
@@ -17,11 +17,11 @@
 import os
 import sys
 import datetime
-import pymysql
+
 import argparse
 
 #User Stack
-import io_utils.ConfigParserLocal as ConfigParserLocal
+from io_utils.EcoFOCI_db_io import EcoFOCI_db_datastatus
 
 __author__   = 'Shaun Bell'
 __email__    = 'shaun.bell@noaa.gov'
@@ -31,3 +31,17 @@ __version__  = "0.1.0"
 __status__   = "Development"
 
 
+"""------------------------------------------------------------------------------------"""
+parser = argparse.ArgumentParser(description='MySQL database --> .ptr file')
+parser.add_argument('SiteID', metavar='SiteID', type=str,
+	help='Base Site ID')
+parser.add_argument('year', metavar='datatype', type=int,
+	help="year")
+parser.add_argument('MooringID', metavar='MooringID', nargs='+', type=str,
+	help='list of mooring ids')
+
+args = parser.parse_args()
+
+
+EcoFOCI_db = EcoFOCI_db_datastatus()
+(db,cursor) = EcoFOCI_db.connect_to_DB()
