220c683231ca7974829c7a4f3af9624b713e17bb
shaunwbell <shaun.w.bell@gmail.com>
2017-07-20 11:20:55 -0700

    add more cruises to scripts

---
+++ scripts/DB_add_DY1705.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Purpose:
+#       Script to run EcoRAIDMooringMetaData2ptr for each grouping of deployments
+
+
+prog_dir="/Users/bell/Programs/Python/EcoFOCI_PavlofMetaDB/"
+data_dir="/Users/bell/ecoraid/2017/CTDcasts/dy1705/initial_archive/*.nc"
+
+
+first_cast=0
+for files in $data_dir
+do
+	if [ ${first_cast} -eq 0 ]
+	then
+		echo "Adding new table based on :$files"
+		echo "processing file: $files"
+		python ${prog_dir}EcoFOCI_NetCDF2DB.py ${files} -c
+		python ${prog_dir}EcoFOCI_NetCDF2DB.py ${files}
+		first_cast=1
+	else
+		echo "processing file: $files"
+		python ${prog_dir}EcoFOCI_NetCDF2DB.py ${files}
+	fi
+done
+
