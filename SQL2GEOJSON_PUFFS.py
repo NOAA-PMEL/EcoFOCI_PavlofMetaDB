3d90e64150039baabea85abf2c6a938b43760acd
Shaun Bell <shaun.bell@noaa.gov>
2021-06-21 16:03:23 -0700

    add meta-creators

---
+++ SQL2GEOJSON_PUFFS.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+
+"""
+SQL2GEOJSON_PUFFS.py
+ 
+export all ctd cast locations to geojson format for plotting
+
+
+"""
+
+#System Stack
+import datetime
+import argparse
+import pymysql
+import sys
+
+#Science Stack
+import numpy as np
+
+#User Stack
+import utilities.ConfigParserLocal as ConfigParserLocal
+
+
+__author__   = 'Shaun Bell'
+__email__    = 'shaun.bell@noaa.gov'
+__created__  = datetime.datetime(2015, 5, 28)
+__modified__ = datetime.datetime(2015, 5, 28)
+__version__  = "0.1.0"
+__status__   = "Development"
+__keywords__ = 'moorings','csv','google maps', 'heatmap', 'geojson'
+
+"""--------------------------------SQL Init----------------------------------------"""
+
+def connect_to_DB(host, user, password, database):
+    # Open database connection
+    try:
+        db = pymysql.connect(host, user, password, database)
+    except:
+        print("db error")
+        
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+    return(db,cursor)
+
+
+def close_DB(db):
+    # disconnect from server
+    db.close()
+    
+def read_floats(db, cursor, table):
+    sql = "SELECT * from `%s`" % (table)
+    #print sql
+    
+    result_dic = {}
+    try:
+        # Execute the SQL command
+        cursor.execute(sql)
+        # Get column names
+        rowid = {}
+        counter = 0
+        for i in cursor.description:
+            rowid[i[0]] = counter
+            counter = counter +1 
+        #print rowid
+        # Fetch all the rows in a list of lists.
+        results = cursor.fetchall()
+        for row in results:
+            result_dic[row['id']] ={keys: row[keys] for val, keys in enumerate(row.keys())} 
+        return (result_dic)
+    except:
+        print("Error: unable to fecth data")
+
+
+"""------------------------------- MAIN ----------------------------------------"""
+
+parser = argparse.ArgumentParser(description='Cruise Database -> GEOJSON')
+parser.add_argument('--geojson', action='store_true', help='create geojson file')
+
+args = parser.parse_args()
+
+#get information from local config file - a json formatted file
+db_config = ConfigParserLocal.get_config('../db_connection_config_files/db_config_drifters.pyini')
+
+(db,cursor) = connect_to_DB(db_config['host'], db_config['user'], db_config['password'], db_config['database'])
+data_puffs = read_floats(db, cursor, 'popupfloats_deployed')
+
+if args.geojson:
+    ### "Generating .geojson"
+    geojson_header = (
+        '{\n'
+        '"type": "FeatureCollection",\n'
+        '"features": [\n'
+        '{\n'
+        '"type": "Feature",\n'
+        '"geometry": {\n'
+        '"type": "MultiPoint",\n'
+        '"coordinates": [ '
+        )
+    geojson_point_coords = ''
+
+    for a_ind in sorted(data_puffs.keys()):
+        point_coords = point_coords + (f'new google.maps.LatLng({data_puffs[a_ind]['DEPLOY_LAT']},{data_puffs[a_ind]['DEPLOY_LON']})')
+
+        if (a_ind != sorted(data_puffs.keys())[-1]):
+            geojson_point_coords = geojson_point_coords + ', '
+
+    geojson_tail = (
+        ']\n'
+        '},\n'
+        '"properties": {\n'
+        '"prop0": "value0"\n'
+        '}\n'
+        '}\n'
+        ']\n'
+        '}\n'
+        )
+    
+    print(f'{geojson_header}{geojson_point_coords}{geojson_tail}') 
+    
